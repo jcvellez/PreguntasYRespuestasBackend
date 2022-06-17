6889f48da514e42ede11406b1c24e19c25c1baa7
jcvellez <jcvellez@live.com>
2022-06-16 22:51:19 -0500

    Estamos en BackEnd. Me toca la clase 43

---
+++ BackEnd/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        // GET: DefaultController
+        [HttpGet]
+        public string Get()
+        {
+            return "Aplicacion corriendo";
+        }
+    }
+}
